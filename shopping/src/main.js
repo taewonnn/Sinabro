8a0b7aca164147f4ff1bb965c9f320226ef8c5e2
TaeWon Park <thisis_tw94@naver.com>
2024-01-24 22:24:29 +0900

    cart

--- shopping/src/main.js
+++ shopping/src/main.js
@@ -1,16 +1,35 @@
 // https://learnwitheunjae.dev/api/sinabro-js/ecommerce
 
-async function main() {
-  const response = await fetch('https://learnwitheunjae.dev/api/sinabro-js/ecommerce');
-  const products = await response.json();
-  console.log("data : ", products);
+import test from '/src/test.json?raw';
 
+async function getProducts() {
+  if (process.env.NODE_ENV === 'development') {
+    return JSON.parse(test);
+  } else {
+    const response = await fetch('https://learnwitheunjae.dev/api/sinabro-js/ecommerce');
+    return await response.json()
+  }
+}
+
+async function main() {
+  console.log('💡', process.env.NODE_ENV);   // 💡 development
+
+  // if( process.env.NODE_ENV === 'development')
+
+  const products = await getProducts();
+  console.log('products', products)
 
   document.querySelector('#products').innerHTML = 
   products.map((product) => `
     <div class='product'>
       <img src='${product.images[0]}' alt='Image of ${product.name}' />
       <p>${product.name}</p>
+      <div class='flex items-center justify-between'>
+        <span>Price : ${product.regularPrice}</span>
+        <button type='button' class='bg-green-200 hover:bg-green-300 text-green-800 px-4 py-1 rounded-full'>-</button>
+        <span class='text-green-800 hidden'>3</span>
+        <button type='button' class='bg-green-200 hover:bg-green-300 text-green-800 px-4 py-1 rounded-full'>+</button>
+      </div>
     </div>
   `
   ).join('')
