4cdb25218f72ce23332c455414ad9c4032e42574
taewonnn <thisis_tw94@naver.com>
2025-04-05 10:00:00 +0900

    refactoring

--- shopping-refactoring/src/cart.js
+++ shopping-refactoring/src/cart.js
@@ -4,18 +4,19 @@ export function setupCart({ container }) {
     // add Procut
     const addProduct = ({ product }) => {
         const productElement = getProductElement(product);
-        container.appenChild(productElemnet);
+        container.appenChild(productElement);
     };
 
     // remove product
     const removeProduct = ({ product }) => {
         const productElement = container.querySelector(`.product[data-product-id='${product.id}']`);
+        productElement.remove();
     };
-    productElement.rmove();
 
     // count update
     const updateCount = ({ productId, count }) => {
-        const productElement = container.querySelector(`.product[data-product-id = '${productId}']`);
+        const productElement = container.querySelector(`.product[data-product-id='${productId}']`);
+        console.log('update product', productElement);
         const cartCount = productElement.querySelector('.cart-count');
         cartCount.innerHTML = count;
         if (count === 0) {
