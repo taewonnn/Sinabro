dc9d181e1d5477b1f26addd4efb1e02cbedb3de2
taewonnn <thisis_tw94@naver.com>
2025-04-06 10:00:00 +0900

    refactoring

--- shopping-refactoring/src/counter.js
+++ shopping-refactoring/src/counter.js
@@ -1,3 +1,28 @@
 export const setupCounter = () => {
     const countMap = {};
+
+    const increase = ({ productId }) => {
+        console.log('+');
+
+        if (countMap[productId] === undefined) {
+            countMap[productId] = 0;
+        }
+        countMap[productId] += 1;
+
+        // return 최종값
+        return countMap[productId];
+    };
+
+    const decrease = ({ productId }) => {
+        console.log('-');
+        if (countMap[productId] === undefined) {
+            countMap[productId] = 0;
+        }
+        countMap[productId] -= 1;
+
+        // return 최종값
+        return countMap[productId];
+    };
+
+    return { increase, decrease };
 };
