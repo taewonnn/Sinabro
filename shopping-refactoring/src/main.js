9ba59be4b8df2fbecf0dd3548fcb0b4fb95421f8
taewonnn <thisis_tw94@naver.com>
2025-04-24 14:22:29 +0900

    refactoring

--- shopping-refactoring/src/main.js
+++ shopping-refactoring/src/main.js
@@ -3,19 +3,7 @@
 import { setupCart } from './cart';
 import { setupCounter } from './counter';
 import { setupProducts, getProductElement } from './products';
-
-// decrease / increase 눌렀는데 어떤 상품을 누른 건지 알아야 하기위해!
-// 어떤 상품인지 찾는 함수
-function findElement(startingElement, selector) {
-    let currentElement = startingElement;
-    while (currentElement) {
-        if (currentElement.matches(selector)) {
-            return currentElement;
-        }
-        currentElement = currentElement.parentElement;
-    }
-    return null;
-}
+import { findElement } from './util';
 
 async function main() {
     const { updateCount: updateProductCount, getProductById } = await setupProducts({ container: document.querySelector('#products') });
