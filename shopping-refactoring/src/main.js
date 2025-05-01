60e3c4f6ed4278677908aa5253ebd3048af234b8
taewonnn <thisis_tw94@naver.com>
2025-05-01 10:00:00 +0900

    refactoring

--- shopping-refactoring/src/main.js
+++ shopping-refactoring/src/main.js
@@ -1,15 +1,6 @@
 // https://learnwitheunjae.dev/api/sinabro-js/ecommerce
 
-import test from '/src/test.json?raw';
-
-async function getProducts() {
-    if (process.env.NODE_ENV === 'development') {
-        return JSON.parse(test);
-    } else {
-        const response = await fetch('https://learnwitheunjae.dev/api/sinabro-js/ecommerce');
-        return await response.json();
-    }
-}
+import { setupProducts, getProductHTML } from './products';
 
 // decrease / increase 눌렀는데 어떤 상품을 누른 건지 알아야 하기위해!
 // 어떤 상품인지 찾는 함수
@@ -39,38 +30,8 @@ function sumAllCounts(countMap) {
     }, 0);
 }
 
-// 장바구니 담은 상품 HTML
-function getProductHTML(product, count = 0) {
-    return `
-        <div class='product' data-product-id='${product.id}'>
-            <img src='${product.images[0]}' alt='Image of ${product.name}' />
-            <p>${product.name}</p>
-            <div class='flex items-center justify-between'>
-                <span>Price : ${product.regularPrice}</span>
-                <button type='button' class='btn-decrease disabled:cursor-not-allowed disabled:opacity-50 bg-green-200 hover:bg-green-300 text-green-800 px-4 py-1 rounded-full'>-</button>
-                <span class='cart-count text-green-800'>${count === 0 ? '' : count}</span>
-                <button type='button' class='btn-increase bg-green-200 hover:bg-green-300 text-green-800 px-4 py-1 rounded-full'>+</button>
-            </div>
-        </div>
-    `;
-}
-
 async function main() {
-    console.log('💡', process.env.NODE_ENV); // 💡 development
-
-    // 데이터
-    const products = await getProducts();
-    const productMap = {};
-
-    // count update
-    const updateProductCount = (productId) => {
-        const productElement = document.querySelector(`.product[data-product-id = '${productId}']`);
-        const cartCount = productElement.querySelector('.cart-count');
-        cartCount.innerHTML = countMap[productId];
-        if (countMap[productId] === 0) {
-            cartCount.innerHTML = '';
-        }
-    };
+    const { updateCount } = await setupProducts({ container: document.querySelector('#products') });
 
     // 장바구니 내용물 업데이트 + Cart 옆 숫자 업데이트
     const updateCart = () => {
@@ -97,7 +58,7 @@ async function main() {
         }
 
         countMap[productId] += 1;
-        updateProductCount(productId);
+        updateCount({ productId, count: countMap[productId] });
         updateCart();
     };
 
@@ -108,19 +69,12 @@ async function main() {
         }
 
         countMap[productId] -= 1;
-        updateProductCount(productId);
+        updateCount({ productId, count: countMap[productId] });
         updateCart();
     };
 
-    products.forEach((product) => {
-        productMap[product.id] = product;
-    });
-    console.log('products', products);
-
     // count 저장
     const countMap = {};
-
-    document.querySelector('#products').innerHTML = products.map((product, index) => getProductHTML(product)).join('');
 
     // 방법1 - 버튼마다 이벤트 붙여주기
     // Array.from(document.querySelectorAll('.btn-decrease')).forEach(button => {
---
+++ shopping-refactoring/src/products.js
@@ -0,0 +1,51 @@
+import test from '/src/test.json?raw';
+
+async function getProducts() {
+    if (process.env.NODE_ENV === 'development') {
+        return JSON.parse(test);
+    } else {
+        const response = await fetch('https://learnwitheunjae.dev/api/sinabro-js/ecommerce');
+        return await response.json();
+    }
+}
+
+// 장바구니 담은 상품 HTML
+export function getProductHTML(product, count = 0) {
+    return `
+        <div class='product' data-product-id='${product.id}'>
+            <img src='${product.images[0]}' alt='Image of ${product.name}' />
+            <p>${product.name}</p>
+            <div class='flex items-center justify-between'>
+                <span>Price : ${product.regularPrice}</span>
+                <button type='button' class='btn-decrease disabled:cursor-not-allowed disabled:opacity-50 bg-green-200 hover:bg-green-300 text-green-800 px-4 py-1 rounded-full'>-</button>
+                <span class='cart-count text-green-800'>${count === 0 ? '' : count}</span>
+                <button type='button' class='btn-increase bg-green-200 hover:bg-green-300 text-green-800 px-4 py-1 rounded-full'>+</button>
+            </div>
+        </div>
+    `;
+}
+
+export async function setupProducts({ container }) {
+    // 데이터
+    const products = await getProducts();
+    const productMap = {};
+    products.forEach((product) => {
+        productMap[product.id] = product;
+    });
+
+    console.log('products', products);
+
+    document.querySelector('#products').innerHTML = products.map((product, index) => getProductHTML(product)).join('');
+
+    // count update
+    const updateCount = ({ productId, count }) => {
+        const productElement = container.querySelector(`.product[data-product-id = '${productId}']`);
+        const cartCount = productElement.querySelector('.cart-count');
+        cartCount.innerHTML = count;
+        if (count === 0) {
+            cartCount.innerHTML = '';
+        }
+    };
+
+    return { updateCount };
+}
